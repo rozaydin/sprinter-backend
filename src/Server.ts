b081db7a065b77667b1e57731e01f6aba81ee0f3
rozaydin <ridvanozaydin@gmail.com>
2019-05-13 08:49:48 +0300

    implemented company operations

--- src/Server.ts
+++ src/Server.ts
@@ -6,12 +6,23 @@ const companyRepo = new CompanyRepository();
 
 const typeDefs = gql(`
 
+     type Company {
+         id: ID!
+         name: String!
+         logo: String
+     }
+
      type Query {
-         sayName: String!
+         // Company Query Operations         
+         getCompany(id: ID!): Company!
+         getAllCompanies: [Company!]!
+         getCompanyWith(field: String!, value: String): [Company]!
      }     
 
      type Mutation {
-         newCompany(name: String!, logo: String): Int!
+         // Company Mutations
+         newCompany(name: String!, logo: String): Company!
+         updateCompany(id: ID!, name: String!, logo: String ): Company!
      }
 
 `);
@@ -19,30 +30,48 @@ const typeDefs = gql(`
 const resolvers = {
 
     Query: {
-        sayName: ()=> { return "Ridvan" },
+        async getCompany(_, { id }) {
+            const response = await companyRepo.get(id);
+            return response;
+        },
+
+        async getAllCompanies(_) {
+            return await companyRepo.getAll();
+        },
+
+        async getCompanyWith(_, {field, value}) {
+            console.log(field);
+            console.log(value);
+            const response = await companyRepo.getWith(field, value);
+            console.log(response);
+            return response;
+        }
+
     },
     //
     Mutation: {
-        async newCompany(_, {name, logo}) {
 
-            console.log("Received request!");
-            console.log(name);
-            console.log(logo);
-            //
+        async newCompany(_, { name, logo }) {
             const response = await companyRepo.save(new CompanyImpl(name, logo));
-            return response.insertId;            
+            const companyEntitiy = await companyRepo.get(response.insertId);
+            return companyEntitiy;
         },
+
+        async updateCompany(_, { id, name, logo }) {            
+            await companyRepo.update(id, new CompanyImpl(name, logo));                        
+            return await companyRepo.get(id);            
+        }
     }
 
 };
 
 const server = new ApolloServer({
-    typeDefs, 
+    typeDefs,
     resolvers
 });
 
 server
-.listen()
-.then((resp) => {
-    console.log(resp);
-});
+    .listen()
+    .then((resp) => {
+        console.log(resp);
+    });
